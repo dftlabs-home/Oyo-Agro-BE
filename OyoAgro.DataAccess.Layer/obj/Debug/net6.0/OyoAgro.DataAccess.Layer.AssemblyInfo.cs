4178a287e6e6885470b470fad7d8bca8a245144e
Malik <malik.s@allprimetech.com>
2025-10-05 18:13:53 +0100

    new

--- OyoAgro.DataAccess.Layer/obj/Debug/net6.0/OyoAgro.DataAccess.Layer.AssemblyInfo.cs
+++ OyoAgro.DataAccess.Layer/obj/Debug/net6.0/OyoAgro.DataAccess.Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OyoAgro.DataAccess.Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68af5d3f4a79a5dc7cd4f22365b024ea6ff948bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9737f85c3c62c89842aa6b7b15b053dfd07902d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("OyoAgro.DataAccess.Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OyoAgro.DataAccess.Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
